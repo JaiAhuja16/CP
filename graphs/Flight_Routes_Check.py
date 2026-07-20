f7f6ffee29681b062c47b91778660a5f295c461a
JaiAhuja16 <jai24275@iiitd.ac.in>
2026-07-20 23:36:12 +0530

    added .gitignore

---
+++ graphs/Flight_Routes_Check.py
@@ -0,0 +1,12 @@
+import sys, math, bisect
+from collections import defaultdict
+input = sys.stdin.readline
+ 
+sin, mulin, intin, arrin = lambda: input().strip(), lambda: map(int,input().split()), lambda: int(input()), lambda: list(map(int,input().split()))
+
+def solve():
+    n = intin()
+    
+    
+for _ in range(intin()):
+    solve()
