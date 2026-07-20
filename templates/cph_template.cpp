325ef7fcb112c6c59d8adf31dd55abfb362010d7
JaiAhuja16 <jai24275@iiitd.ac.in>
2026-07-20 23:31:57 +0530

    init repo

---
+++ templates/cph_template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define int long long int
+using namespace std;
+
+void solve(){
+    
+}
+
+int32_t main() {
+    int t;
+    cin >> t;
+	while (t--){
+	    solve();
+	    cout<<'\n';
+	}
+    return 0;
+}
